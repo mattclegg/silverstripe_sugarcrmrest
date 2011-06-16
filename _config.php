f5e398640552a98caea4e0bbfef523e4a3700f7f
mattclegg <cleggmatt@gmail.com>
2011-06-16 08:44:27 -0700

    Edited _config.php via GitHub

--- _config.php
+++ _config.php
@@ -1,6 +1,3 @@
 <?php
 
 DataObject::add_extension('Member', 'Member_SugarCRM');
-
-SugarRestfulService::set_url('http://andromeda.enableit.org.uk/solihull/service/v3_1/rest.php');
-SugarRestfulService::set_username_password('matt','enableit');
