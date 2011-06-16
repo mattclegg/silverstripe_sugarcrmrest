d2ee05074ba2ca20142fb43d779bd2892bb42286
MattClegg <cleggmatt@gmail.com>
2011-06-16 18:02:27 +0200

    Initial commit (part deux)

--- _config.php
+++ _config.php
@@ -2,5 +2,5 @@
 
 DataObject::add_extension('Member', 'Member_SugarCRM');
 
-SugarRestfulService::set_url('http://andromeda.enableit.org.uk/solihull/service/v3_1/rest.php');
-SugarRestfulService::set_username_password('matt','enableit');
+SugarRestfulService::set_url('your sugar url');
+SugarRestfulService::set_username_password('username','password');
