a1d6d227b4ebc500b70da4088c04b404abcb64a6
mattclegg <cleggmatt@gmail.com>
2011-12-09 12:29:51 +0000

    upload notes
    added option to upload files to note attachments for a user, and tidied
    up code

--- code/Member_SugarCRM.php
+++ code/Member_SugarCRM.php
@@ -20,6 +20,16 @@ class Member_SugarCRM extends DataObjectDecorator {
 		        "birthdate"=>"Date",
 				"last_date_worked_c"=>"Date",
 				"special_requirements_c"=>'Text',
+
+			"job_type_sought_other"=>"Text",
+			"job_type_sought_sic_other"=>"Text",
+		
+		
+			"cv_profile"	=>"Text",
+			"cv_skills"	=>"Text",
+			"cv_history"	=>"Text",
+			"cv_education"	=>"Text",
+			"cv_interests"=>"Text"
         	),
         	'has_one'=>array(
 				"ethnicity"=>"Member_SugarCRM_ethnicity",
@@ -30,7 +40,11 @@ class Member_SugarCRM extends DataObjectDecorator {
         	),
         	'many_many'=>array(
         		"barriers_to_employment"=>"Member_SugarCRM_barriers_to_employment",
-        	)
+        		"JobsAppliedFor"=>'GEOmodule_Job'
+        	),
+        	'defaults'=>array(
+				"sugar_id"=>"0",
+			)
 		);
 	}
 	
@@ -50,6 +64,18 @@ class Member_SugarCRM extends DataObjectDecorator {
 			}else{
 				$x->update_member($member->sugar_id,$member,$member->getChangedFields());
 			}
+			
+			
+			if($member->UploadedCV()){
+				if(!$member->sugar_note_id){
+					$member->sugar_note_id=$x->create_note($member->sugar_id,$member->UploadedCV());
+				}else{
+					if($raw_file=$x->validate_file_for_upload($member->UploadedCV())){
+						$x->update_note($member->sugar_note_id,$member->UploadedCV(),$raw_file);
+					}
+				}
+			}
+			
 		}
 		parent::onBeforeWrite();
 	}
@@ -81,12 +107,22 @@ class Member_SugarCRM extends DataObjectDecorator {
 			"birthdate"=>"birthdate",
 			"last_date_worked_c"=>"last_date_worked_c",
 			"job_type_sought_c"=>"job_type_sought_c",
+			"job_type_sought_other"=>"job_type_sought_other",
+			"job_type_sought_sic_other"=>"job_type_sought_sic_other",
+		
+		
 			"special_requirements_c"=>"special_requirements_c",
 			"data_protection_c"=>"data_protection_c",
+			"area_c"=>"area_c",
+
+			"cv_profile_c"	=>"cv_profile",
+			"cv_skills_c"	=>"cv_skills",
+			"cv_history_c"	=>"cv_history",
+			"cv_education_c"=>"cv_education",
+			"cv_interests_c"=>"cv_interests"
 		);
 	
 	}
-	
 	
 }
 
--- code/SugarRestfulService.php
+++ code/SugarRestfulService.php
@@ -34,8 +34,12 @@ class SugarRestfulService extends RestfulService {
 		        ),
 		    );
 		if($result=$this->run('login',$data)){
-			self::$session_id=$result->id;
-			return true;
+		
+			if(isset($result->id)){
+			
+				self::$session_id=$result->id;
+				return true;
+			}
 		}
 	}
 	
@@ -43,64 +47,37 @@ class SugarRestfulService extends RestfulService {
 		return self::$session_id;
 	}
 	
-	/**
-	
-	public function get_entry(){	
-	
-	
-	$parameters = array(
-	    'session' => $this->sessionId,
-	    'module' => 'sh_Clients',
-	    'name_value_list' => array(
-	        array('name' => 'name', 'value' => 'New Account'),
-	        array('name' => 'description', 'value' => 'This is an account created from a REST web services call'),
-        ),
-    );
-	
-	$json = json_encode($parameters);
-	$postArgs = array(
-	    'method' => 'get_entry',
-	    'input_type' => 'JSON',
-	    'response_type' => 'JSON',
-	    'rest_data' => $json,
-    );
-	get_entry
-	
-	
-	}
-	**/
-	
-	public function run($method,$data){
-	
+	public function run($method,$data,$input_type='JSON'){
+
+		$rest_data=($input_type=='JSON')?json_encode($data):serialize($data);
+
 		$this->setQueryString($data);
-	
 		if($x=parent::request(null, "POST", array(
-		    'method' => $method,
-		    'input_type' => 'JSON',
-		    'response_type' => 'JSON',
-		    'rest_data' => json_encode($data),
-	    ), null,array())){
-	    	return json_decode($x->getBody());
-	    }
+			'method' => $method,
+			'input_type' => $input_type,
+			'response_type' => 'JSON',
+			'rest_data' => $rest_data,
+		), null,array())){
+			return json_decode($x->getBody());
+		}
 	}
-	
-	
+
 	public function find_member($email){
 	
 		$data = array(
-		    'session' => $this->sessionId(),
-		    'module' => 'sh_Clients',
-		    'query' => "(sh_clients.id in (select eabr.bean_id from email_addr_bean_rel eabr join email_addresses ea on eabr.email_address_id = ea.id where eabr.bean_module=\"sh_clients\" and ea.email_address LIKE \"$email\" and ea.deleted=\"0\" and eabr.deleted=\"0\"))",
+			'session' => $this->sessionId(),
+			'module' => 'sh_Clients',
+			'query' => "(sh_clients.id in (select eabr.bean_id from email_addr_bean_rel eabr join email_addresses ea on eabr.email_address_id = ea.id where eabr.bean_module=\"sh_clients\" and ea.email_address LIKE \"$email\" and ea.deleted=\"0\" and eabr.deleted=\"0\"))",
 			'order_by' => 'sh_clients.id',
 			'offset'	=> 0,
 			'select_fields'=>'id',
 			'link_name_to_fields_array'=>null,
-			'max_results'=>'100',
+			'max_results'=>'1',
 			'deleted'=>false
 		);
 
 		if($result=$this->run('get_entry_list',$data)){
-			if($result->total_count>0){
+			if($result->result_count>0){
 				foreach ($result->entry_list as $entry){
 					return $entry->id;
 				}
@@ -115,7 +92,7 @@ class SugarRestfulService extends RestfulService {
 		$data = array(
 		    'session' => $this->sessionId(),
 		    'module' => 'sh_Clients',
-		    'name_value_list' => $this->set_field_map($member, null,array(
+		    'name_value_list' => $this->set_field_map($member, $member->getAllFields(),array(
 				"source"=>"Web Site"
 			))
 		);
@@ -138,8 +115,99 @@ class SugarRestfulService extends RestfulService {
 			)
 		);
 		if($result=$this->run('set_entry',$data)){
-			Debug::show($result);
+			return $result->id;
+		}else{
+			Log::log("Could not create Member:{$member->ID} on sugar", "1");
+		}
+	}
+
+	public function validate_file_for_upload($file){
 		
+		$target_path=$file->getFullPath();
+		
+		if(@file_exists($target_path)){
+			$fp = fopen ($target_path, "r");
+			$raw_file = base64_encode(fread($fp, filesize($target_path)));
+			fclose($fp);
+			return $raw_file;
+		}else{
+			return false;
+		}
+	}
+	
+	public function create_note($bean_id,$file){
+
+		if($raw_file = validate_file_for_upload($file)){
+			
+			$data = array(
+				'session' => $this->sessionId(),
+				'module' => 'Notes',
+				'name_value_list' => array(
+					"name"=>$file->Title,
+					"parent_type"=>'sh_Clients',
+					"parent_id"=>$bean_id
+				)
+			);
+
+			if($result=$this->run('set_entry',$data)){
+				return update_note($result->id,$file,$raw_file);
+			}
+		}
+	}
+
+	public function update_note($noteId,$file,$raw_file){
+	
+		$this->setQueryString(array(
+			'method' => 'set_note_attachment',
+			'input_type' => 'JSON',
+			'response_type' => 'JSON',
+		));
+		
+		if($x=parent::request(null, "POST", array(
+			'rest_data'=>json_encode(array(
+			'session' => $this->sessionId(),
+			'attachment'=>array(
+				"id" => $noteId,
+				"filename" => "{$file->Title}.{$file->getExtension()}",
+				"file" => $raw_file,
+			)
+		))))){
+			$result=json_decode($x->getBody());
+			return $result->id;
+		}else{
+			Debug::show($result);
+			Debug::show('set_note_attachment failed');
+		}
+
+	}
+
+	public function create_job($job){
+
+		$data = array(
+			'session' => $this->sessionId(),
+			'module' => 'geo_GEO',
+			'name_value_list' => $this->set_field_map($job, $job->SugarFieldLookup())
+		);
+		
+		if($result=$this->run('set_entry',$data)){	
+			return $result->id;
+		}else{
+			Log::log("Could not create Job:{$job->ID} on sugar", "1");
+		}
+	}
+	
+	public function update_job($bean_id,$member,$fields){
+
+		$data = array(
+		    'session' => $this->sessionId(),
+		    'module' => 'geo_GEO',
+		    'name_value_list' => $this->set_field_map($member, $member->SugarFieldLookup(),array(
+					"id"=>$bean_id
+				)
+			)
+		);
+		
+		if($result=$this->run('set_entry',$data)){
 			return $result->id;
 		}else{
 			Log::log("Could not create Member:{$member->ID} on sugar", "1");
@@ -147,14 +215,30 @@ class SugarRestfulService extends RestfulService {
 	}
 	
 	
+	public function apply_for_job($member_sugar,$job_sugar){
+	
+		$data = array(
+		    'session' => $this->sessionId(),
+			'module_name'=>'sh_Clients',
+			'module_id'=>$member_sugar,
+			'link_field_name'=>'sh_clients_geo_geo',
+			'related_ids'=>array($job_sugar)
+		);
+	
+		if($result=$this->run('set_relationship',$data)){
+			Debug::show($data);
+			Debug::show($result);
+			return $result->created;
+		}else{
+			Debug::show("Member:{$member} could not apply for {$job} on sugar", "1");
+		}
+	}
 	
 	
 	public function set_field_map($member,$filter=array(),$details=array("source"=>"Web Site")){
 	
 	
 		$has_ones=$member->has_one();
-		
-
 		foreach($member->SugarFieldLookup() as $fieldName => $fieldType) {
 
 			if (in_array($fieldType,$filter)){
@@ -168,11 +252,18 @@ class SugarRestfulService extends RestfulService {
 						}	
 					
 					}else{
-						$details[]=array('name'=>$fieldName,'value'=>$member->$fieldType);
+						
+						if ($member->obj($fieldType) instanceof HTMLText) {
+							$value=Convert::html2raw($member->$fieldType);
+						}else{
+							$value=$member->$fieldType;
+						}
+						$details[]=array('name'=>$fieldName,'value'=>$value);
 					}
 				}
 			}
 		}
+		
 		return $details;
 	}
 	
